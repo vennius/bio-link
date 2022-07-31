63da7f960d4edd525a0c6e9002475bb5aeb0c775
Stevennius Chandra <stevenniuschandra@gmail.com>
2022-07-31 16:23:31 +0700

    add birthday countdown and edit description

--- components/Description.js
+++ components/Description.js
@@ -1,11 +1,31 @@
 import { useEffect } from "react";
 import styles from "../styles/components/Description.module.css";
+import { isPast, getMinutes, getSeconds, getHours, getDaysInMonth } from 'date-fns';
 import AOS from "aos";
 
 export default function Description({text}){
   useEffect(() => {
     AOS.init();
   }, []);
+  
+  function getBirthdayCountdown(birthdayDate, birthdayMonth){
+    
+    const now = new Date();
+    const birthday = (isPast(new Date(now.getFullYear(), birthdayMonth, birthdayDate))) ? new Date(now.getFullYear()+1, birthdayMonth, birthdayDate) : new Date(now.getFullYear(), birthdayMonth, birthdayDate);
+    const restMonth = (now.getFullYear() == birthday.getFullYear()) ? (12 - now.getMonth() - (12-birthdayMonth)) : (12 - now.getMonth() + birthday.getMonth());
+    
+    if(restMonth != 0) return {
+      month: restMonth
+    };
+    else return {
+      date: birthday.getDate() - now.getDate()
+    }
+    
+  }
+  
+  console.log(getBirthdayCountdown(26, 8-1));
+  
+  const date = getBirthdayCountdown(26, 8-1);
   
   return (
     
@@ -17,16 +37,28 @@ export default function Description({text}){
         </div>
         
         <div className={styles.containerContent}>
-        My programing languages: <b>Javascript, PHP</b>👨‍💻
+        I'm from <b>Indonesia</b>🇮🇩
         </div>
         
         <div className={styles.containerContent}>
-        My frameworks: <b>Express.js, Next.js</b>🎞️
+        I'm <b>X</b> years old ✌️
         </div>
         
         <div className={styles.containerContent}>
-        Databases Im using: <b>MySQL, MongoDB</b>📂
+        Birthday on <b>26 August</b> 🎂
         </div>
+        
+        <div className={styles.birthdayWrapper}>
+        My next birthday:
+        </div>
+        <p style={{
+          fontWeight: "bold",
+          fontSize: "1.6em",
+          padding: 0,
+          margin: 0,
+          taxtAlign: "center"
+        }}>{(date.month) ? `${date.month} ${(date.month > 1) ? "Months left" : "Month left"}` : `${date.date} ${(date.date > 1) ? "Days left" : "Day left"}`}</p>
+
         
       </div>
     </div>
--- components/Name.js
+++ components/Name.js
@@ -6,6 +6,22 @@ export default function Name({name}){
       fontWeight: "bold",
       fontSize: "1.5em",
       letterSpacing: 1
+    }} onClick={(e) => {
+      navigator.permissions.query({name: "clipboard-write"})
+    .then(({state}) => {
+        console.log(`permission response: ${state}`);
+        if (state === "granted") {
+            const data = [new ClipboardItem({ "text/plain": new Blob([name], { type: "text/plain" }) })]
+            navigator.clipboard.write(data).then(
+                () => {
+                    alert("Name copied!");
+                },
+                () => {
+                    alert("Name not copied, something went wrong!");
+                }
+            );
+        }
+    });
     }}>@{name}</div>
   
   );
